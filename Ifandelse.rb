742d103408bf157d596ef1c3998085efa3d1fd3e
Duke Ko <dukhyunko93@gmail.com>
2020-12-03 16:10:08 +0000

    Automatically backed up by Learn

---
+++ Ifandelse.rb
@@ -0,0 +1,10 @@
+puts "Hello! What is your name!"
+name = gets.chomp
+
+if name == "Duke"
+  puts "Duke Hello!"
+elsif name == "James"
+  puts "James, go home!"
+else
+  puts "Hello my friend"
+end
---
+++ learning-comments.rb
@@ -0,0 +1,5 @@
+puts (1)
+
+# From the Three Dog Night Song "Joy to the World"
+puts "Joy to the world"
+puts "All the Boys and girls"
